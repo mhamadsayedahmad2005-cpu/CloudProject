796fa8d439e8b42db6e01efa07e997ba0cb8c62b
Mohamad Sayed Ahmad <mhamadsayedahmad2005@gmail.com>
2025-11-23 12:52:21 +0200

    Update DB

--- connect.php
+++ connect.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
-$DB_HOST = "localhost";
-$DB_USER = "root";
-$DB_PASS = "";
-$DB_NAME = "isd";
+$host = "localhost";
+$username = "clouduser";
+$password = "StrongPassword123!";
+$database = "isd";
+
 
 $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
